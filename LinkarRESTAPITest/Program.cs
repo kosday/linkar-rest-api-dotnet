1bd5c2e061c01c14be418e03db47f9563a66197b
kosday <47530763+kosday@users.noreply.github.com>
2020-03-16 09:00:06 +0100

    Add files via upload

--- LinkarRESTAPITest/Program.cs
+++ LinkarRESTAPITest/Program.cs
@@ -11,9 +11,11 @@ namespace LinkarRESTAPITest
             try
             {
                 HttpClient client = new HttpClient();
-                HttpContent httpContentJSON = new StringContent("{ \"APIKey\" : \"my_apikey\", \"Data\" : { \"CALCULATED\" : \"False\", \"DICTIONARIES\" : \"False\", \"CONVERSION\" : \"False\", \"FORMAT_SPEC\" : \"False\", \"ORIGINAL_RECORDS\" : \"False\" ,\"CUSTOM_VARS\" : \"\" ,\"OUTPUT_FORMAT\" : \"MV\" ,\"FILE_NAME\" : \"LK.CUSTOMERS\" ,\"RECORD_IDS\" : [ { \"ID\" : \"2\" }, { \"ID\" : \"3\" } ] ,\"RECORD_DICTCLAUSE\" : \"\"} }", Encoding.UTF8, "application/json");
-                HttpContent httpContentXML = new StringContent("<LkCommand><APIKey>my_apikey</APIKey><Data><CALCULATED>False</CALCULATED><DICTIONARIES>False</DICTIONARIES><CONVERSION>False</CONVERSION><FORMAT_SPEC>False</FORMAT_SPEC><ORIGINAL_RECORDS>False</ORIGINAL_RECORDS><CUSTOM_VARS></CUSTOM_VARS><OUTPUT_FORMAT>MV</OUTPUT_FORMAT><FILE_NAME>LK.CUSTOMERS</FILE_NAME><RECORD_IDS><ID>2</ID><ID>3</ID></RECORD_IDS><RECORD_DICTCLAUSE></RECORD_DICTCLAUSE></Data></LkCommand>", Encoding.UTF8, "text/xml");
-                HttpResponseMessage response = client.PostAsync("http://yourdomain.com:11201/api/read", httpContentXML).Result;
+				//JSON
+                //HttpContent httpContent = new StringContent("{ \"APIKey\" : \"my_apikey\", \"Data\" : { \"CALCULATED\" : \"False\", \"DICTIONARIES\" : \"False\", \"CONVERSION\" : \"False\", \"FORMAT_SPEC\" : \"False\", \"ORIGINAL_RECORDS\" : \"False\" ,\"CUSTOM_VARS\" : \"\" ,\"OUTPUT_FORMAT\" : \"MV\" ,\"FILE_NAME\" : \"LK.CUSTOMERS\" ,\"RECORD_IDS\" : [ { \"ID\" : \"2\" }, { \"ID\" : \"3\" } ] ,\"RECORD_DICTCLAUSE\" : \"\"} }", Encoding.UTF8, "application/json");
+                //XML 
+				HttpContent httpContent = new StringContent("<LkCommand><APIKey>my_apikey</APIKey><Data><CALCULATED>False</CALCULATED><DICTIONARIES>False</DICTIONARIES><CONVERSION>False</CONVERSION><FORMAT_SPEC>False</FORMAT_SPEC><ORIGINAL_RECORDS>False</ORIGINAL_RECORDS><CUSTOM_VARS></CUSTOM_VARS><OUTPUT_FORMAT>MV</OUTPUT_FORMAT><FILE_NAME>LK.CUSTOMERS</FILE_NAME><RECORD_IDS><ID>2</ID><ID>3</ID></RECORD_IDS><RECORD_DICTCLAUSE></RECORD_DICTCLAUSE></Data></LkCommand>", Encoding.UTF8, "text/xml");
+                HttpResponseMessage response = client.PostAsync("http://yourdomain.com:11201/api/read", httpContent).Result;
                 response.EnsureSuccessStatusCode();
                 string responseBody = response.Content.ReadAsStringAsync().Result;
 
