f43727a6855a734295d7b225f0f861d976c6d557
kosday <47530763+kosday@users.noreply.github.com>
2020-06-30 15:56:39 +0200

    Update to LinkarClient 2.0.0.7

--- LinkarRESTAPITest/Program.cs
+++ LinkarRESTAPITest/Program.cs
@@ -12,9 +12,9 @@ namespace LinkarRESTAPITest
             {
                 HttpClient client = new HttpClient();
 				//JSON
-                //HttpContent httpContent = new StringContent("{ \"APIKey\" : \"my_apikey\", \"Data\" : { \"CALCULATED\" : \"False\", \"CONVERSION\" : \"False\", \"FORMAT_SPEC\" : \"False\", \"ORIGINAL_RECORDS\" : \"False\" ,\"CUSTOM_VARS\" : \"\" ,\"OUTPUT_FORMAT\" : \"MV\" ,\"FILE_NAME\" : \"LK.CUSTOMERS\" ,\"RECORDS\" : [ { \"LKITEMID\" : \"2\" }, { \"LKITEMID\" : \"3\" } ] ,\"DICT_CLAUSE\" : \"\"} }", Encoding.UTF8, "application/json");
+                //HttpContent httpContent = new StringContent("{ \"APIKey\" : \"my_apikey\", \"Data\" : { \"CALCULATED\" : \"False\", \"CONVERSION\" : \"False\", \"FORMAT_SPEC\" : \"False\", \"ORIGINAL_RECORDS\" : \"False\" ,\"CUSTOM_VARS\" : \"\" ,\"OUTPUT_FORMAT\" : \"JSON\" ,\"FILE_NAME\" : \"LK.CUSTOMERS\" ,\"RECORDS\" : [ { \"LKITEMID\" : \"2\" }, { \"LKITEMID\" : \"3\" } ] ,\"DICT_CLAUSE\" : \"\"} }", Encoding.UTF8, "application/json");
                 //XML 
-				HttpContent httpContent = new StringContent("<LkCommand><APIKey>my_apikey</APIKey><Data><CALCULATED>False</CALCULATED><CONVERSION>False</CONVERSION><FORMAT_SPEC>False</FORMAT_SPEC><ORIGINAL_RECORDS>False</ORIGINAL_RECORDS><CUSTOM_VARS></CUSTOM_VARS><OUTPUT_FORMAT>MV</OUTPUT_FORMAT><FILE_NAME>LK.CUSTOMERS</FILE_NAME><RECORDS><RECORD><LKITEMID>2</LKITEMID></RECORD><RECORD><LKITEMID>3</LKITEMID></RECORD></RECORDS><DICT_CLAUSE></DICT_CLAUSE></Data></LkCommand>", Encoding.UTF8, "text/xml");
+				HttpContent httpContent = new StringContent("<LkCommand><APIKey>my_apikey</APIKey><Data><CALCULATED>False</CALCULATED><CONVERSION>False</CONVERSION><FORMAT_SPEC>False</FORMAT_SPEC><ORIGINAL_RECORDS>False</ORIGINAL_RECORDS><CUSTOM_VARS></CUSTOM_VARS><OUTPUT_FORMAT>JSON</OUTPUT_FORMAT><FILE_NAME>LK.CUSTOMERS</FILE_NAME><RECORDS><RECORD><LKITEMID>2</LKITEMID></RECORD><RECORD><LKITEMID>3</LKITEMID></RECORD></RECORDS><DICT_CLAUSE></DICT_CLAUSE></Data></LkCommand>", Encoding.UTF8, "text/xml");
                 HttpResponseMessage response = client.PostAsync("http://yourdomain.com:11201/api/read", httpContent).Result;
                 response.EnsureSuccessStatusCode();
                 string responseBody = response.Content.ReadAsStringAsync().Result;
